95461d003a3d2e2ca931b1cca6face627ea51b2e
Alphonso84 <Alphonso.SensleyJR@gmail.com>
2018-07-27 01:33:39 -0700

    Updating UI to work on multiple screen sizes

--- Sketch Weather/AppDelegate.swift
+++ Sketch Weather/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreLocation
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -32,6 +33,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        manager.delegate = self as! CLLocationManagerDelegate
+        manager.requestAlwaysAuthorization()
+        manager.requestWhenInUseAuthorization()
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.startUpdatingLocation()
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
